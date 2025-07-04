48c73d6c739b549f8bea954966d45ff43d1d8836
jacobmichels <jacob.michels2025@gmail.com>
2025-07-03 23:33:08 -0400

    add option to surpress fish's greeting message

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -538,6 +538,12 @@ in
         '';
       };
 
+      suppressGreeting = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Suppress the greeting message in interactive shells.";
+      };
+
       shellInitLast = mkOption {
         type = types.lines;
         default = "";
@@ -772,6 +778,9 @@ in
           end
 
           status is-interactive; and begin
+
+            # Suppress greeting
+            ${lib.optionalString cfg.suppressGreeting "set fish_greeting"}
 
             # Abbreviations
             ${abbrsStr}
