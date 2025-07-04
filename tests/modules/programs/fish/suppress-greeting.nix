5b43e4dfff7e40804e9f1c12f6c3b3fc4c2da2f4
jacobmichels <jacob.michels2025@gmail.com>
2025-07-03 23:33:08 -0400

    add option to surpress fish's greeting message

---
+++ tests/modules/programs/fish/suppress-greeting.nix
@@ -0,0 +1,16 @@
+_: {
+  config = {
+    programs.fish = {
+      enable = true;
+      suppressGreeting = true;
+    };
+
+    nmt = {
+      description = "if fish.suppressGreeting is set, check fish.config contains the correct setting";
+      script = ''
+        assertFileContains home-files/.config/fish/config.fish \
+          "set fish_greeting"
+      '';
+    };
+  };
+}
